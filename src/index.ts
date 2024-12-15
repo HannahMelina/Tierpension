70dd5f156d4a46bdff4502fe22304ae18f98e80e
Hannah Patsch <hannah.patsch@dakitec.de>
2024-12-15 12:47:54 +0100

    add animal png

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import * as ex from 'excalibur';
 
 const game = new ex.Engine({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
 });
 
 const player = new ex.Actor({
-    x: 100,
-    y: 100,
+    x: 200,
+    y: 200,
     width: 50,
     height: 50,
     color: ex.Color.Red,
 });
 
+const dog = new ex.Actor({
+    x: 400,
+    y: 100,
+    width: 50,
+    height: 50,
+    color: ex.Color.Black,
+});
+
 game.add(player);
+game.add(dog);
 
 game.start();
